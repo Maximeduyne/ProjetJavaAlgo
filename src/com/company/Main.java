972bd0604acf7b6aced3c160362bae6b597fa89f
maximeduyne <maximeduyne@icloud.com>
2021-09-29 19:56:01 +0200

    creation du menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        int choice = -1;
+        printMenu();
+        choice = scan.nextInt();
+
+        while(choice != 4){
+            switch(choice){
+                case 1 :
+                    System.out.println("Lancement du jeu");
+                    break;
+
+                case 2 :
+                    System.out.println("Affichage des règles");
+                    break;
+
+                case 3 :
+                    System.out.println("Affichage des Scores");
+            }
+            printMenu();
+            choice = scan.nextInt();
+        }
+        System.out.println("Merci d'avoir jouer");
+    }
+        public static void printMenu() {
+
+            System.out.println("Bienvenue :\n"
+            + "1)Commencer le jeu\n"
+            + "2)Afficher les règles\n"
+            + "3)Afficher les scores\n"
+            + "4)Quitter le Jeu");
     }
 }
